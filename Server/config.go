cac47ce6d39cb9f0cdf3022be2b20e1b2caa3c0f
Taki Do <ferrusi.enzo06@gmail.com>
2025-10-22 12:12:54 +0200

    Server Config

--- Server/config.go
+++ Server/config.go
@@ -3,4 +3,7 @@ package Server
 const PORT = ":4545"
 const ADMIN_USERNAME = "Admin"
 const ADMIN_PASSWORD = "Phaistos2025!"
+
 const GAME_ROUTE = "/play"
+const ADMIN_ROUTE = "/admin"
+const AUTOR_ROUTE = "/autor"
